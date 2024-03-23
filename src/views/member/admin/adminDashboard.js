f2c7c7eaa05dce61f5acad0a8f6ba52366c0439b
Arnav Bhasin <arnavbhasin@wireless-10-155-77-220.public.utexas.edu>
2024-03-23 14:59:16 -0500

    admin dashboard done

--- src/views/member/admin/adminDashboard.js
+++ src/views/member/admin/adminDashboard.js
@@ -36,43 +36,43 @@ function AdminDashboard() {
     };
 
     return (
-        <div className="overflow-auto p-4">
-            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <div className="h-screen overflow-auto p-4">
+            <table className="rounded-lg overflow-hidden w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <th scope="col" className="py-3 px-6">User ID</th>
-                        <th scope="col" className="py-3 px-6">First Name</th>
-                        <th scope="col" className="py-3 px-6">Last Name</th>
-                        <th scope="col" className="py-3 px-6">Roblox ID</th>
-                        <th scope="col" className="py-3 px-6">Instructor</th>
-                        <th scope="col" className="py-3 px-6">Last Completed Module</th>
-                        <th scope="col" className="py-3 px-6">Actions</th>
+                        <th scope="col" className="font-bold text-white text-sm py-3 px-6">User ID</th>
+                        <th scope="col" className="font-bold text-white text-sm py-3 px-6">First Name</th>
+                        <th scope="col" className="font-bold text-white text-sm py-3 px-6">Last Name</th>
+                        <th scope="col" className="font-bold text-white text-sm py-3 px-6">Roblox ID</th>
+                        <th scope="col" className="font-bold text-white text-sm py-3 px-6">Instructor</th>
+                        <th scope="col" className="font-bold text-white text-sm py-3 px-6">Last Completed Module</th>
+                        <th scope="col" className="font-bold text-white text-sm py-3 px-6">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                     {users.map((user) => (
                         <tr key={user.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <td className="py-4 px-6">{user.id}</td>
-                            <td className="py-4 px-6">{user.firstname}</td>
-                            <td className="py-4 px-6">{user.lastname}</td>
-                            <td className="py-4 px-6">{user.roblox_id}</td>
-                            <td className="py-4 px-6">{user.instructor || 'N/A'}</td>
-                            <td className="py-4 px-6">{user.last_completed_module}</td>
+                            <td className="text-gray-200 py-4 px-6">{user.id}</td>
+                            <td className="text-gray-200 py-4 px-6">{user.firstname}</td>
+                            <td className="text-gray-200 py-4 px-6">{user.lastname}</td>
+                            <td className="text-gray-200 py-4 px-6">{user.roblox_id}</td>
+                            <td className="text-gray-200 py-4 px-6">{user.instructor || 'N/A'}</td>
+                            <td className="text-gray-200 py-4 px-6">{user.last_completed_module}</td>
                             <td className="py-4 px-6 flex justify-start items-center space-x-4">
                                 <button 
-                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                                    className="transition-all duration-100 border-2 border-green-500 bg-gray-800 hover:bg-green-500 text-green-500 hover:text-white font-bold py-2 px-4 rounded"
                                     onClick={() => incrementModule(user.id, user.last_completed_module)}
                                 >
                                     Increment Module
                                 </button>
                                 <button 
-                                    className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
+                                    className="transition-all duration-100 border-2 border-red-500 bg-gray-800 hover:bg-red-500 text-red-500 hover:text-white font-bold py-2 px-4 rounded"
                                     onClick={() => decrementModule(user.id, user.last_completed_module)}
                                 >
                                     Decrement Module
                                 </button>
                                 <button 
-                                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                                    className="transition-all duration-100 bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-4 rounded"
                                     onClick={() => deleteUser(user.id)}
                                 >
                                     Delete User
